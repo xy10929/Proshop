1440e838f76234bb49186c371000f32325d65360
Zhengyu Yang <yangzy0105@gmail.com>
2023-09-03 10:53:15 -0700

    9.3

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,25 @@
 import { useParams } from 'react-router-dom'
-import products from '../products'
+// import products from '../products'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({})
+
   const { id: productId } = useParams()
-  const product = products.find((p) => p._id === productId)
+  // const product = products.find((p) => p._id === productId)
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`)
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [productId])
 
   return (
     <>
