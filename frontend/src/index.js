707ce2d5242778918ff42674eb4e5cf6557f9170
Zhengyu Yang <yangzy0105@gmail.com>
2023-09-03 03:17:03 -0700

    9.3

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,12 +13,15 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       {/* show HomeScreen when route is exactly / */}
       <Route index={true} path='/' element={<HomeScreen />} />
+
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 )
