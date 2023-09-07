b9d5847eb1367db75dab97b93f5cc52b011f4ca1
Zhengyu Yang <yangzy0105@gmail.com>
2023-09-07 13:25:17 -0700

    9.7

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,8 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 
@@ -29,7 +31,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
 
