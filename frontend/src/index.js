bf787e48b3bd34af020da1f103a94788f22342d6
Zhengyu Yang <yangzy0105@gmail.com>
2023-09-07 13:38:20 -0700

    9.7

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,10 +12,10 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import store from './store'
+import { Provider } from 'react-redux'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
