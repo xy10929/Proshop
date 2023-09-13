88520e668a3f72444aac3e9dcefa92a7a8ac314e
Zhengyu Yang <yangzy0105@gmail.com>
2023-09-12 23:16:28 -0700

    9.12

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from '../controllers/userController.js'
+
+const router = express.Router()
+
+router.route('/').get(getUsers).post(registerUser)
+router.post('/logout', logoutUser)
+router.post('/login', authUser)
+router.route('/profile').get(getUserProfile).put(updateUserProfile)
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser)
+
+export default router
