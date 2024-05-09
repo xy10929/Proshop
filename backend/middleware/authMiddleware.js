f8201e5682e731367c8dd067710dc7a9811d4643
Zhengyu Yang <yangzy0105@gmail.com>
2024-05-08 20:00:32 -0700

    5.8

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken'
 import asyncHandler from './asyncHandler.js'
 import User from '../models/userModel.js'
 
+//verify and get loged in user from req, then add user into req
 const protect = asyncHandler(async (req, res, next) => {
   let token
 
