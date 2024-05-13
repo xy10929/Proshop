0607dc99a192705ddfbd859dca4fd53df47ba8ce
Zhengyu Yang <yangzy0105@gmail.com>
2024-05-12 17:11:35 -0700

    5.12

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -4,6 +4,7 @@ import FormContainer from '../components/FormContainer'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { saveShippingAddress } from '../slices/cartSlice'
+import CheckoutSteps from '../components/CheckoutSteps'
 
 const ShippingScreen = () => {
   //try to get shippingAddress from state
@@ -32,6 +33,8 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
+
       <h1>Shipping</h1>
 
       <Form onSubmit={submitHandler}>
