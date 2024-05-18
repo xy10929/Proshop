dfbd8f0ee3b0c518d2e536606958cd6e21b9f7f9
Zhengyu Yang <yangzy0105@gmail.com>
2024-05-17 23:13:48 -0700

    5.17

--- frontend/src/slices/productApiSlice.js
+++ frontend/src/slices/productApiSlice.js
@@ -10,15 +10,27 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       keepUnusedDataFor: 5,
       //keep data cached for 5s when a component querys an endpoint, the value will be served if another component makes the same request within the time
     }),
+
     getProductDetails: builder.query({
       query: (productId) => ({
         url: `${PRODUCTS_URL}/${productId}`,
       }),
       keepUnusedDataFor: 5,
     }),
+
+    createProduct: builder.mutation({
+      query: () => ({
+        url: PRODUCTS_URL,
+        method: 'POST',
+      }),
+      invalidatesTags: ['Product'],
+    }),
   }),
 })
 
-export const { useGetProductsQuery, useGetProductDetailsQuery } =
-  productsApiSlice
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+} = productsApiSlice
 //add use... and ...Query
