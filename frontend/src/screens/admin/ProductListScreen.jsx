bd53e0ad20f4f79420049ddb25130ffa8b740134
Zhengyu Yang <yangzy0105@gmail.com>
2024-05-18 23:39:04 -0700

    5.18

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -6,6 +6,7 @@ import Loader from '../../components/Loader'
 import {
   useGetProductsQuery,
   useCreateProductMutation,
+  useDeleteProductMutation,
 } from '../../slices/productApiSlice'
 import { toast } from 'react-toastify'
 
@@ -15,8 +16,21 @@ const ProductListScreen = () => {
   const [createProduct, { isLoading: loadingCreate }] =
     useCreateProductMutation()
 
-  const deleteHandler = (id) => {
-    console.log(id)
+  const [deleteProduct, { isLoading: loadingDelete }] =
+    useDeleteProductMutation()
+
+  const deleteHandler = async (id) => {
+    if (window.confirm('Are you sure you want to delete the product?')) {
+      try {
+        await deleteProduct(id)
+
+        refetch()
+
+        toast.success('Product deleted')
+      } catch (err) {
+        toast.error(err?.data?.message || err.message)
+      }
+    }
   }
 
   const createProductHandler = async () => {
@@ -43,8 +57,7 @@ const ProductListScreen = () => {
         </Col>
       </Row>
 
-      {loadingCreate && <Loader />}
-      {isLoading ? (
+      {isLoading || loadingCreate || loadingDelete ? (
         <Loader />
       ) : error ? (
         <Message variant='danger'>{error}</Message>
--- frontend/src/slices/productApiSlice.js
+++ frontend/src/slices/productApiSlice.js
@@ -1,4 +1,4 @@
-import { PRODUCTS_URL,UPLOAD_URL } from '../constants'
+import { PRODUCTS_URL, UPLOAD_URL } from '../constants'
 import { apiSlice } from './apiSlice'
 
 export const productsApiSlice = apiSlice.injectEndpoints({
@@ -47,6 +47,14 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         body: data,
       }),
     }),
+
+    deleteProduct: builder.mutation({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+        method: 'DELETE',
+      }),
+      providesTags: ['Product'],
+    }),
   }),
 })
 
@@ -55,6 +63,7 @@ export const {
   useGetProductDetailsQuery,
   useCreateProductMutation,
   useUpdateProductMutation,
-  useUploadProductImageMutation
+  useUploadProductImageMutation,
+  useDeleteProductMutation,
 } = productsApiSlice
 //add use... and ...Query
